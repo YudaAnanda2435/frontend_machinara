f0b200383ec03cf0743ceb7272bd389cd7e854d0
YudaAnanda245 <0803yudha@gmail.com>
2025-12-15 00:48:29 +0700

    progres

--- src/components/fragments/banner.jsx
+++ src/components/fragments/banner.jsx
@@ -8,14 +8,14 @@ const Banner = () => {
           <div className="flex flex-col gap-[18px] md:gap-[26px] w-full max-w-[500px] lg:p-[60px]">
             <h3 className="text-[30px] font-medium">
               {" "}
-              "Start Predicting, Stop Reacting.
+              Start Predicting, Stop Reacting.
             </h3>
             <p>
               Book a demo today to see how Machinara’s AI can help your factory
               run without interruption and reduce maintenance costs.
             </p>
             <Buttons
-              className={"buttonAnimate w-fit"}
+              className={"buttonAnimate w-fit"} 
               text={`Get your free proposal`}
             />
           </div>
